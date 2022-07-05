9fc6096f7fc99913ffd1f49621e9fd14ee177617
Ajari Jules <ajarinjules@gmail.com>
2022-07-05 11:42:34 -0400

    Add comments and handle for no arguments

--- check_leaked_passwords.py
+++ check_leaked_passwords.py
@@ -3,16 +3,19 @@ import hashlib
 import sys
 
 PWNED_API_URL = 'https://api.pwnedpasswords.com/range/'
+#Check if password found in list of dictionaries 
 def password_check(list, password):
     for item in list:
         if item['hash'] == password:
             return(item)
     return(None)
 
+#Convert string into SHA1 uppercase hashed string equivalent  
 def get_sha1_pass(password):    
     sha1_pass = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     return sha1_pass
 
+#Get the list of hashes from pwned api by supplying first 5 characters of the hashed password (K anonymity)
 def get_pwnd_api_data(sha1_pass):
     first_five=  sha1_pass[0:5]
     url = f'{PWNED_API_URL}/{first_five[0:5]}' 
@@ -28,18 +31,20 @@ def get_pwnd_api_data(sha1_pass):
     full_hashed_list = [ {'hash': first_five + item['hash'] , 'count': item['count']} for item in suffixes_list]
     return  full_hashed_list           
 
-    
 def main():
     passwords = (sys.argv[1:])
-    for password in passwords:
-        hashed_pass = get_sha1_pass(password)
-        pwnd_passwords = get_pwnd_api_data(hashed_pass)
-        pass_check = password_check(pwnd_passwords,hashed_pass)
+    if passwords:
+        for password in passwords:
+            hashed_pass = get_sha1_pass(password)
+            pwnd_passwords = get_pwnd_api_data(hashed_pass)
+            pass_check = password_check(pwnd_passwords,hashed_pass)
 
-        if pass_check:
-            print(f'Password {password} leaked { pass_check["count"] } times,  consider using a different password')
-        else:
-            print(f'Password {password} was never leaked, should be safe :) ')
+            if pass_check:
+                print(f'Password {password} leaked { pass_check["count"] } times,  consider using a different password')
+            else:
+                print(f'Password {password} was never leaked, should be safe :) ')
+    else: 
+        print('Please enter one or more passwords as arguments USAGE - python check_leaked_passwords.py password1 password2 password3 ')
             
 if __name__ == '__main__':
     main()
